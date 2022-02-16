80189917d94424d63b371d7bb150192d1b9b6e46
Walid-Ouldali <ouldali02@hotmail.fr>
2022-02-16 19:34:21 +0100

    Adding icon

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -10,7 +10,7 @@ export default function Intro() {
 			showCursor: true,
 			backDelay: 1500,
 			backSpeed: 60,
-			strings: [ 'Student', 'Fullstack Developper', 'Future Software engineer' ]
+			strings: [ 'Student', 'Fullstack Developer', 'Future Software engineer' ]
 		});
 	}, []);
 
